969c96dc07103d0207d4f1518aa6b2b238b4e3bc
dvtgtt <td-msaa@yandex.ru>
2016-10-26 15:40:28 +0500

    1, 2

---
+++ Delegates/Delegates/Task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class TransactionProcessor
+    {
+        Func<TransactionRequest, bool> Check;
+        Func<TransactionRequest, Transaction> Register;
+        Action<Transaction> Save;
+
+        public TransactionProcessor(Func<TransactionRequest, bool> check,
+            Func<TransactionRequest, Transaction> register,
+            Action<Transaction> save)
+        {
+            Check = check;
+            Register = register;
+            Save = save;
+        }
+
+        public Transaction Process(TransactionRequest request)
+        {
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+    }
+
+    public class Transaction
+    {
+    }
+
+    public class TransactionRequest
+    {
+    }
+}
---
+++ Delegates/Delegates/Task2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Task2
+    {
+        public class TransactionProcessor<Request, Object>
+        {
+            Func<Request, bool> Check;
+            Func<Request, Object> Register;
+            Action<Object> Save;
+
+            public TransactionProcessor(Func<Request, bool> check,
+                Func<Request, Object> register,
+                Action<Object> save)
+            {
+                Check = check;
+                Register = register;
+                Save = save;
+            }
+
+            public Object Process(Request request)
+            {
+                if (!Check(request))
+                    throw new ArgumentException();
+                var result = Register(request);
+                Save(result);
+                return result;
+            }
+        }
+    }
+}
